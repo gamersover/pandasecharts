d65646d385335fcc9e246a60afbcef54c2732c94
gamersover <cmathking@gmail.com>
2021-12-11 22:12:02 +0800

    update version to 0.2.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/guides/single-sourcing-package-version/
-    version='0.1.0',  # Required
+    version='0.2.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -145,7 +145,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/discussions/install-requires-vs-requirements/
     install_requires=[
-        'pandas', 
+        'pandas',
         'pyecharts>=1.0.0'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
